da5f79ef969914ec9bcbfb8ef672e7a0c75eba86
TheWill <willimossop@gmail.com>
2022-03-25 12:32:55 +0300

    cleanup

--- StartupStudio/settings.py
+++ StartupStudio/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_db',
         'USER': 'postgres',
-        'PASSWORD': 'PUTYOURPASSWORDHERE',
+        'PASSWORD': 'INSERTYOURPASSWORDHERE',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
